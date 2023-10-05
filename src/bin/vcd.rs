325876b4cf3a40c3e1b48f6b9bb576675ff3267d
valentinstoecker <valentin@stoeckermail.de>
2023-10-05 01:14:56 +0000

    add selection

--- src/bin/vcd.rs
+++ src/bin/vcd.rs
@@ -1,34 +1,42 @@
 use std::{
-    env,
+    env, fs,
     io::{self, Result, Stdout},
-    path::PathBuf,
 };
 
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use tools::widgets::container::Container;
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+use tools::widgets::{
+    container::Container,
+    list::{List, ListState},
+};
 use tui::{
-    backend::CrosstermBackend, buffer::Buffer, layout::Rect, style::Style, widgets::Widget,
+    backend::CrosstermBackend, buffer::Buffer, layout::Rect, style::Style, widgets::StatefulWidget,
     Terminal,
 };
 
 #[derive(Clone)]
 struct State {
-    path: PathBuf,
+    path: String,
+    subdirs: List,
 }
 
-impl Widget for State {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        buf.set_string(
-            area.x,
-            area.y,
-            self.path.to_str().unwrap().to_string(),
-            Style::default(),
-        )
+impl StatefulWidget for State {
+    type State = ListState;
+    fn render(self, area: Rect, buf: &mut Buffer, state: &mut ListState) {
+        buf.set_string(area.x, area.y, self.path, Style::default());
+        self.subdirs.render(
+            Rect::new(area.x + 2, area.y + 1, area.width - 2, area.height - 1),
+            buf,
+            state,
+        );
     }
 }
 
 struct App {
     state: State,
+    list_state: ListState,
     term: Terminal<CrosstermBackend<Stdout>>,
 }
 
@@ -42,16 +50,35 @@ impl App {
         disable_raw_mode()?;
         let mut term = self.term;
         term.show_cursor()?;
+        term.set_cursor(0, 0)?;
+        term.clear()?;
         Ok(())
     }
 
     fn new() -> Result<Self> {
-        let path = env::current_dir()?;
-        let state = State { path };
+        let path = env::current_dir()?.to_str().unwrap().to_string();
+        let subdirs = fs::read_dir(&path)?
+            .filter_map(|entry| entry.ok())
+            .filter_map(|entry| {
+                entry
+                    .path()
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .map(|s| s.to_string())
+            })
+            .collect::<Vec<String>>();
+        let subdirs = List::new(subdirs);
+        let state = State { path, subdirs };
         let stdout = io::stdout();
         let backend = CrosstermBackend::new(stdout);
         let term = Terminal::new(backend)?;
-        let app = App { state, term };
+        let list_state = ListState::default();
+        let app = App {
+            state,
+            term,
+            list_state,
+        };
         app.init()?;
         Ok(app)
     }
@@ -60,15 +87,51 @@ impl App {
         self.term.clear()?;
         self.term.draw(|f| {
             let widget = Container::new("vcd".to_string(), self.state.clone());
-            f.render_widget(widget, f.size());
+            f.render_stateful_widget(widget, f.size(), &mut self.list_state);
         })?;
         Ok(())
+    }
+
+    fn up(&mut self) {
+        match self.list_state.selected {
+            Some(selected) => {
+                if selected > 0 {
+                    self.list_state.select(selected - 1);
+                }
+            }
+            None => {
+                self.list_state.select(self.state.subdirs.len() - 1);
+            }
+        }
+    }
+
+    fn down(&mut self) {
+        match self.list_state.selected {
+            Some(selected) => {
+                if selected < self.state.subdirs.len() - 1 {
+                    self.list_state.select(selected + 1);
+                }
+            }
+            None => {
+                self.list_state.select(0);
+            }
+        }
     }
 }
 
 fn main() -> Result<()> {
     let mut app = App::new()?;
-    app.draw()?;
+    loop {
+        app.draw()?;
+        if let Event::Key(k) = event::read()? {
+            match k.code {
+                KeyCode::Esc => break,
+                KeyCode::Up => app.up(),
+                KeyCode::Down => app.down(),
+                _ => {}
+            }
+        }
+    }
     app.reset()?;
     Ok(())
 }
---
+++ src/widgets/list.rs
@@ -0,0 +1,75 @@
+use tui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::{Color, Style},
+    widgets::StatefulWidget,
+};
+
+#[derive(Clone)]
+pub struct ListState {
+    pub selected: Option<usize>,
+    offset: usize,
+}
+
+impl ListState {
+    pub fn select(&mut self, index: usize) {
+        self.selected = Some(index);
+    }
+}
+
+impl Default for ListState {
+    fn default() -> Self {
+        Self {
+            selected: None,
+            offset: 0,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct List {
+    items: Vec<String>,
+}
+
+impl List {
+    pub fn new(items: Vec<String>) -> Self {
+        Self { items }
+    }
+
+    pub fn len(&self) -> usize {
+        self.items.len()
+    }
+}
+
+impl StatefulWidget for List {
+    type State = ListState;
+
+    fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
+        // check if the selected item is visible
+        if let Some(selected) = state.selected {
+            if selected < state.offset {
+                state.offset = selected;
+            } else if selected >= state.offset + area.height as usize {
+                state.offset = selected - area.height as usize + 1;
+            }
+        }
+
+        // render items
+        let rendered_items = self
+            .items
+            .iter()
+            .enumerate()
+            .skip(state.offset)
+            .take(area.height as usize)
+            .map(|(i, item)| {
+                let mut style = Style::default();
+                if Some(i) == state.selected {
+                    style = style.fg(Color::Yellow);
+                }
+                (item, style)
+            });
+        for (i, (item, style)) in rendered_items.enumerate() {
+            buf.set_string(area.x, area.y + i as u16, item, style);
+        }
+    }
+}
